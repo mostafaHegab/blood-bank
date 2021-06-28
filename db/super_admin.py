d9daf529f8f1be6411b72445c3281d4416afeff0
Mohamed-Abobakr20 <abobakrmohamed2222@gmail.com>
2021-06-28 21:42:58 +0200

    adding the cityName and the governorateName

--- db/super_admin.py
+++ db/super_admin.py
@@ -1,24 +1,24 @@
-def add_hospitals(name,email,username,password,address, phone, info):
-    query = """INSERT INTO Hospitals(name, email, username, password, address, phone, info)
-	VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}')"""
-    return query.format(name, email, username, password, address, phone, info)
+def add_hospitals(name,email,password, phone, street, cityId, info, createdAt):
+    query = """INSERT INTO Hospitals(name,email,password, phone, street, cityId, info, createdAt)
+	VALUES('{}', '{}', '{}', '{}', '{}', {}, '{}', '{}')"""
+    return query.format(name,email,password, phone, street, cityId, info, createdAt)
 
 #******************************************************
 
-def add_bloodbank(name,email,username,password,address, phone, info):
-    query = """INSERT INTO BloodBanks(name, email, username, password, address, phone, info)
-	VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}')"""
-    return query.format(name, email, username, password, address, phone, info)
+def add_bloodbank(name,email,password,phone, street, cityId, info,createdAt):
+    query = """INSERT INTO BloodBanks(name,email,password,phone, street, cityId, info,createdAt)
+	VALUES('{}', '{}', '{}', '{}', '{}', {}, '{}', '{}')"""
+    return query.format(name,email,password,phone, street, cityId, info,createdAt)
 
 #******************************************************
 
 def get_hospitals():
-    return """SELECT name, email, address, phone, info FROM Hospitals"""
+    return """SELECT name, email, concat(street, Cities.name, Governorates.name), phone, info, createdAt FROM Hospitals INNER JOIN Cities ON Hospitals.cityId = Cities.id INNER JOIN Governorates ON Cities.governorateId = Governorates.id"""
 
 #******************************************************
 
 def get_bloodbanks():
-    return """SELECT name, email, address, phone, info FROM BloodBanks"""
+    return """SELECT name, email, concat(street, Cities.name, Governorates.name), phone, info, createdAt FROM BloodBanks INNER JOIN Cities ON BloodBanks.cityId = Cities.id INNER JOIN Governorates ON Cities.governorateId = Governorates.id"""
 
 #******************************************************
 
