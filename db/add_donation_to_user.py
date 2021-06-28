190a3c00ce4b38af7d553e8942577471f7237cb2
Seleem1010 <Mohammedseleem10101997@gmail.com>
2021-06-28 21:55:47 +0200

    get user donation and apply for donate

--- db/add_donation_to_user.py
+++
@@ -1,3 +0,0 @@
-def Add_User_Donations( id, userid, bloodbankid , status, donationdate, bags, createdat):
- query = """INSERT INTO public.donations(id, userid, bloodbankid, status, donationdate, bags, createdat) VALUES ({},{},{},{},{},{},{})"""
- return query.format(id, userid, bloodbankid, status, donationdate, bags, createdat)
--- db/user_donations.py
+++ db/user_donations.py
@@ -1,3 +1,14 @@
-def Get_User_Donations(id):
+def get_user_donations(id):
  query = """SELECT Donations.id AS DonationId,BloodBanks.name AS BankName,CONCAT(Governorates.name,' , ',Cities.name,' , ',BloodBanks.street) AS Address,donationDate,bags FROM Donations INNER JOIN BloodBanks ON Donations.bloodBankId = BloodBanks.id INNER JOIN Cities ON BloodBanks.cityId = Cities.id INNER JOIN Governorates ON Cities.governorateId = Governorates.id INNER JOIN Users ON Users.id= {}"""
  return query.format(id)
+
+#------------------------------------------------------------------------------------------------------------------------
+
+def add_user_donations(userid, bloodbankid , donationdate, createdat):
+ query = """INSERT INTO public.donations(userid, bloodbankid, status,donationdate, createdat) VALUES ({},{},benging,{},{})"""
+ return query.format(userid, bloodbankid, donationdate, createdat)
+
+#-------------------------------------------------------------------------------------------------------------------------
+def update_user(weight,hasdiseases,lasttreatmentdate,userid):
+ query = """UPDATE public.users SET weight={}, hasdiseases={}, lasttreatmentdate={} WHERE users.id = {}"""
+ return query.format(weight,hasdiseases,lasttreatmentdate,userid)
