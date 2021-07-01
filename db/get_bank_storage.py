c18ef012d4078731c8f10c5585754fd20d61e098
Seleem1010 <Mohammedseleem10101997@gmail.com>
2021-07-01 20:32:46 +0200

    get bank storage

--- db/get_bank_storage.py
+++
@@ -1,2 +0,0 @@
-def get_bank_Storage():
-    return """SELECT id,type,bloodClass,storingDate,expirationDate FROM BloodCases WHERE BloodCases.orderId IS null AND isDeleted = 'False' ORDER BY expirationDate DESC"""
