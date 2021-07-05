b1eec31bad023dfd39d65741f8638776102dbdee
Seleem1010 <Mohammedseleem10101997@gmail.com>
2021-07-05 17:09:31 +0200

    get unserved blood requests

--- db/unserved_blood_requests.py
+++ db/unserved_blood_requests.py
@@ -1,2 +1,3 @@
-def get_unserved_requests():
-    return """SELECT Orders.id,hospitalId ,CONCAT(Governorates.name,' , ',Cities.name,' , ',Hospitals.street) AS Address,amount ,date FROM Orders INNER JOIN Hospitals ON Orders.hospitalId = Hospitals.id INNER JOIN Cities ON Hospitals.cityId = Cities.id INNER JOIN Governorates ON Cities.governorateId = Governorates.id WHERE Orders.status != 'accepted'"""
+def get_unserved_requests(bankId):
+ query = """SELECT Orders.id,Hospitals.name ,CONCAT(Governorates.name,' , ',Cities.name,' , ',Hospitals.street) AS Address,amount ,date FROM Orders INNER JOIN Hospitals ON Orders.hospitalId = Hospitals.id INNER JOIN Cities ON Hospitals.cityId = Cities.id INNER JOIN Governorates ON Cities.governorateId = Governorates.id WHERE orders.bloodBankId = {} AND Orders.status = 'pending'"""
+ return query.format(bankId)
