9d2f0878df181fe496514089df806a54353d72c1
mostafa <mkhedre3@gmail.com>
2021-07-05 20:40:53 +0200

    donation req + get blood case

--- db/donation_request.py
+++ db/donation_request.py
@@ -1,3 +1,3 @@
 def get_donation_request(id):
-    query = """SELECT Users.id, name, birthDate, email, gender, weight, hasDiseases, lastTreatmentDate, bloodClass, lastDonationDate FROM Users INNER JOIN Donations ON Donations.userId = Users.id WHERE donations.bank_id = {} AND Donations.status = 'Pending'"""
+    query = """SELECT Users.id, name, birthDate, email, gender, weight, hasDiseases, lastTreatmentDate, bloodClass, lastDonationDate FROM Users INNER JOIN Donations ON Donations.userId  = Users.id WHERE donations.bloodBankId = {} AND Donations.status = 'pending'"""
     return query.format(id) 
